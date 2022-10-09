11b6117f28d2849f3059623f4e1d862526f6d318
Shauri Maigua <90189750+SCNMC@users.noreply.github.com>
2022-10-09 15:55:55 +0200

    homepage v1.1

--- src/lib/components/Maincontent.svelte
+++ src/lib/components/Maincontent.svelte
@@ -9,7 +9,6 @@
 		<section class="chapters">
 			<section class="beoogde-leerresultaten">
 				<a href="#">1.0 Beoogde leerresultaten</a>
-
 				<ul class="list-categories">
 					<li>1.1 Humanity-centered digital designers</li>
 					<li>1.2 Competenties</li>
@@ -19,19 +18,38 @@
 				</ul>
 			</section>
 
-			<section class="toetsing">
+			<section class="onderwijsleeromgeving">
 				<a href="#">2.0 Onderwijsleeromgeving</a>
-				<span />
+				<ul class="2.0-categories">
+					<li>2.1 Onderwijsvisie en didactiek</li>
+					<li>2.2 Curriculumopbouw</li>
+					<li>2.3 Borging onderwijskwaliteit</li>
+					<li>2.4 Onderwijs en praktijk</li>
+					<li>2.5 Onderwijs en onderzoek</li>
+					<li>2.6 Internationalisering</li>
+					<li>2.8 Digitale en analoge voorzieningen</li>
+					<li>2.9 Ambities en dilemma's</li>
+				</ul>
 			</section>
 
 			<section class="toetsing">
 				<a href="#"> 3.0 Toetsing</a>
-				<span />
+				<ul class="3.0-categories">
+					<li>3.1 Toetsbeleid en -plan</li>
+					<li>3.2 Toetsprogramma en -praktijk</li>
+					<li>3.3 Toetsing en feedback</li>
+					<li>3.4 Borging en kwaliteit</li>
+					<li>3.5 Ambities en dilemma's</li>
+				</ul>
 			</section>
 
 			<section class="gerealiseerde-leerresultaten">
 				<a href="#"> 4.0 Gerealiseerde Leerresultaten</a>
-				<span />
+				<ul class="4.0-categories">
+					<li>4.1 Afstudeerproces</li>
+					<li>4.2 Borging afstudeerniveau vanuit examencommissie</li>
+					<li>4.3 Ambities en dilemma's</li>
+				</ul>
 			</section>
 		</section>
 	</section>
@@ -55,10 +73,10 @@
 		gap: 3em;
 	}
 
-	section:hover ul.list-categories {
+	/* section:hover ul.list-categories {
 		max-height: inherit;
 		opacity: 1;
-	}
+	} */
 	a {
 		font-weight: 600;
 		font-size: 1.3em;
@@ -68,19 +86,20 @@
 	li {
 		padding-top: 1.5em;
 		font-weight: normal;
+		list-style: none;
 	}
 
 	span {
 		border-bottom: 4px solid #000;
 	}
 
-	.list-categories {
+	/* .list-categories {
 		list-style-type: none;
 		padding: 0px;
 		margin: 0px;
 		max-height: 0px;
 		opacity: 0;
 		overflow: hidden;
-		transition: opacity 300ms ease;
-	}
+		transition: opacity 500ms ease-in-out;
+	} */
 </style>
